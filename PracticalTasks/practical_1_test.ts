131652e91b77907f38053ada2095357d67876417
kate-bobryk <kate.bobryk@tealium.com>
2023-10-24 16:09:48 +0200

    secondd commit

---
+++ PracticalTasks/practical_1_test.ts
@@ -0,0 +1,53 @@
+Feature('Practical Task Number 1 - Variables and Type of Data');
+
+Scenario("PZ1 Variables and Type of Data", () => {
+
+let variableA:number = 10;
+console.log(`variableA: ${variableA}, ++variableA: ${++variableA}`);
+console.log(`variableA: ${variableA}, variableA++: ${variableA++}, variableA: ${variableA}`);
+console.log(`variableA: ${variableA}, --variableA: ${--variableA}`);
+console.log(`variableA: ${variableA}, variableA--: ${variableA--}, variableA: ${variableA}`);
+console.log(variableA);
+const variableB:string = "hello";
+const variableC:boolean = true;
+const result: any = variableA + variableB + variableC;
+console.log(`result: ${result}`);
+console.log(`typeof result: ${typeof result}`);
+const variableD = [variableA, variableB, variableC];
+console.log(typeof variableD)
+let variableF = {variableA, variableB, variableC};
+console.log(variableF)
+enum AnswerOptions { Yes, No };
+console.log(`Is your answer ${AnswerOptions[0]} or ${AnswerOptions[1]}?`);
+enum AnswerOptions2 { YES = "Yes", NO = "No" };
+console.log(`Is your answer ${AnswerOptions2.YES} or ${AnswerOptions2.NO}?`);
+const experiment: string[] = [];
+console.log(experiment == null);
+console.log(experiment == undefined);
+const emptyArr: Array<string> = [];
+console.log("Is emptyArr null ? ", !!emptyArr);
+console.log("Is emptyArr undefined ? ", emptyArr == undefined);
+console.log("Is emptyArr[0] undefined ? ", emptyArr[0] == undefined);
+let userInfo:any = {name:"Irina", age:"20", country:"Poland"};
+userInfo = { company:"Tealium" };
+console.log(userInfo.name);
+console.log(userInfo.company);
+type FileType = {
+    fileName: string;
+    filePath: string;
+}
+const newFile:FileType = {fileName: "text.tx", filePath: "./Tests/"};
+console.log(newFile.filePath + newFile.fileName);
+let test : string | number;
+test = 5;
+console.log(typeof test);
+test = "5"
+console.log(typeof test)
+const a2 = null
+console.log(!a2)
+console.log(!!a2)
+console.log(null == undefined);
+console.log(null == 0);
+console.log(false == undefined);
+console.log("" == undefined);
+})
---
+++ examples_test.ts
@@ -0,0 +1,5 @@
+Feature('examples');
+
+Scenario('test something',  ({ I }) => {
+
+});
